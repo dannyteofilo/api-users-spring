6fab44262dea372883a6accc61e20205f5f73ce5
dannyteofilo <danyel.285309@gmail.com>
2023-11-27 10:18:43 -0600

    project initialization

---
+++ src/main/java/com/users/apiusersspring/ApiUsersSpringApplication.java
@@ -0,0 +1,13 @@
+package com.users.apiusersspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsersSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsersSpringApplication.class, args);
+	}
+
+}
