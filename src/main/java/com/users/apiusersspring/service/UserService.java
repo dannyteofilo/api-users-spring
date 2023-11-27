dd5462d30408605c3f4ee4c8ace53fdcaf4ac283
dannyteofilo <danyel.285309@gmail.com>
2023-11-27 11:31:28 -0600

    feat: user controller created

--- src/main/java/com/users/apiusersspring/service/UserService.java
+++ src/main/java/com/users/apiusersspring/service/UserService.java
@@ -3,14 +3,19 @@ package com.users.apiusersspring.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.users.apiusersspring.model.User;
 import com.users.apiusersspring.repository.UserRepository;
 
+@Service
 public class UserService {
+
+    @Autowired
     private UserRepository userRepository;
 
     public List<User> getAllUsers() {
